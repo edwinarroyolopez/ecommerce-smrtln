0fe613a5af1cfa0057a90d4dd825f22f796efa05
Ed <edwin.antonio.arroyo.lopez@gmail.com>
2025-03-08 14:17:55 -0500

    feat: add logger and sentry like apm

--- apps/web/src/store/useAuthStore.ts
+++ apps/web/src/store/useAuthStore.ts
@@ -2,10 +2,11 @@ import { create } from "zustand";
 import { AuthState, User, UserCredentials } from "@src/types/auth";
 import { mockData } from "@/data/products";
 import { getLocalStorageItem, setLocalStorageItem, removeLocalStorageItem } from "@/utils/localStorageUtil";
+import logger from "@/utils/logger";
 
 export const useAuthStore = create<AuthState>((set) => ({
     user: getLocalStorageItem("user", null),
-    isAuthenticated: !!getLocalStorageItem("user",null),
+    isAuthenticated: !!getLocalStorageItem<User | null>("user", null),
 
     login: (credentials: UserCredentials) => {
         const { username } = credentials;
@@ -21,9 +22,16 @@ export const useAuthStore = create<AuthState>((set) => ({
         }
 
         set({ user, isAuthenticated: true });
+
+        logger.log(`User logged in: ${username}, Role: ${user.role}`);
     },
 
     logout: () => {
+        const currentUser = getLocalStorageItem<User | null>("user", null);
+        if (currentUser) {
+            logger.log(`User logged out: ${currentUser.username}`);
+        }
+
         removeLocalStorageItem("user");
         set({ user: null, isAuthenticated: false });
     }
--- apps/web/src/utils/localStorageUtil.ts
+++ apps/web/src/utils/localStorageUtil.ts
@@ -1,12 +1,13 @@
-const getLocalStorageItem = <T,>(key: string, initialValue: T): T => {
-    try {
+const getLocalStorageItem = <T>(key: string, initialValue: T | null): T | null => {
+  try {
       const item = localStorage.getItem(key);
       return item ? (JSON.parse(item) as T) : initialValue;
-    } catch (error) {
+  } catch (error) {
       console.error("Error reading localStorage", error);
       return initialValue;
-    }
-  };
+  }
+};
+
   
   const setLocalStorageItem = <T,>(key: string, value: T): void => {
     try {
