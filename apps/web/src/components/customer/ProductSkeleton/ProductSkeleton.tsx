964af076ab8cc5f3047390240bb152d6c7fd63d1
Ed <edwin.antonio.arroyo.lopez@gmail.com>
2025-03-08 11:10:41 -0500

    feat: add product skeleton

---
+++ apps/web/src/components/customer/ProductSkeleton/ProductSkeleton.tsx
@@ -0,0 +1,16 @@
+import styles from "./ProductSkeleton.module.css";
+
+const ProductSkeleton = () => {
+  return (
+    <div className={styles.skeletonCard}>
+      <div className={`${styles.skeletonBlock} ${styles.skeletonImage}`} />
+      <div className={styles.skeletonContent}>
+        <div className={`${styles.skeletonBlock} ${styles.skeletonText} ${styles.short}`} />
+        <div className={`${styles.skeletonBlock} ${styles.skeletonText} ${styles.medium}`} />
+        <div className={`${styles.skeletonBlock} ${styles.skeletonButton}`} />
+      </div>
+    </div>
+  );
+};
+
+export default ProductSkeleton;
--- apps/web/src/pages/customer/products.tsx
+++ apps/web/src/pages/customer/products.tsx
@@ -2,18 +2,19 @@ import { useEffect, useState } from "react";
 import { getLocalStorageItem } from "@/utils/localStorageUtil";
 import { Product } from "@/types/product";
 import ProductCard from "@/components/customer/ProductCard/ProductCard";
+import ProductSkeleton from "@/components/customer/ProductSkeleton/ProductSkeleton";
 import FloatCart from "@components/customer/FloatCart/FloatCart";
 import styles from "./products.module.css";
 
 const Products = () => {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[] | null>(null);
 
   useEffect(() => {
-    const storedProducts = getLocalStorageItem<Product[]>("products", []);
-    setProducts(storedProducts);
+    setTimeout(() => {
+      const storedProducts = getLocalStorageItem<Product[]>("products", []);
+      setProducts(storedProducts);
+    }, 2000); // Simula carga de 2 segundos
   }, []);
-
-  
 
   return (
     <div className={styles.container}>
@@ -22,13 +23,15 @@ const Products = () => {
         <h1>Productos</h1>
       </header>
       <div className={styles.productsGrid}>
-        {products.length > 0 ? (
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))
-        ) : (
-          <p>No hay productos disponibles</p>
-        )}
+        {products === null
+          ? Array.from({ length: 6 }).map((_, index) => (
+              <ProductSkeleton key={index} />
+            ))
+          : products.length > 0
+          ? products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))
+          : <p>No hay productos disponibles</p>}
       </div>
     </div>
   );
