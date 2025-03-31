d3e8ad6bf81e81f6bcbba95b853f1d0556cadec3
Ed <edwin.antonio.arroyo.lopez@github.com>
2025-03-30 20:33:37 -0500

    change endpoint

--- apps/web/src/api/products.ts
+++ apps/web/src/api/products.ts
@@ -1,10 +1,10 @@
 import { Product } from '@src/types/product';
 import axios from 'axios';
 
+  // baseURL: 'http://localhost:5000',
 // Creamos una instancia de axios para el cliente HTTP
 const httpClient = axios.create({
-  // baseURL: 'http://localhost:5000',
-  baseURL: 'scrpng-app-production.up.railway.app',
+  baseURL: 'https://scrpng-app-production.up.railway.app',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
